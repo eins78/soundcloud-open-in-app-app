44a36d238d30296bc2bc15a38ed4f258c8e0242d
Max F. Albrecht <1@178.is>
2013-12-22 14:20:20 +0100

    add 'hello world' server
    (proof-of-concept)

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+var port = process.env.PORT || 5000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, '127.0.0.1');
