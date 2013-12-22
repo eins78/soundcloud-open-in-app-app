e5bc348cc4e2f478fd40b7e0dc6e2a21f4f1dc29
Max F. Albrecht <1@178.is>
2013-12-22 14:42:08 +0100

    server: use heroku's 'hello world'

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var http = require('http');
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(port, '127.0.0.1');
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
