e7743ac9b2445a5c6f8636bd622811c2c69cbad5
Max F. Albrecht <1@178.is>
2014-08-24 00:07:42 +0200

    fix bookmarklet  (v0.3.0)

--- soundcloud-open-in-app-bookmarklet.js
+++ soundcloud-open-in-app-bookmarklet.js
@@ -1,3 +1,5 @@
 // soundcloud-open-in-app-bookmarklet
-var a = window.location.href.replace('soundcloud.com', 'soundcloud-open-in-app.herokuapp.com');
-window.location = a;
+var a = window.location.href.replace(/(https?:\/\/)?.*?soundcloud.com(.*)/, 'https://soundcloud-open-in-app.herokuapp.com$2');
+if (a) {
+  window.location = a;
+}
