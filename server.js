428d61349f9c14bffc21e8d6cfe665346c17bd7a
Max F. Albrecht <1@178.is>
2014-08-23 22:40:11 +0200

    modularize

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var logfmt = require("logfmt");
-var request = require("request");
+var sc = require("./lib/soundcloud-api");
 
 // init express app
 var app = express();
@@ -35,46 +35,6 @@ app.listen(port, function() {
   console.log("Listening on " + port);
   console.log("SC id " + process.env.SC_client_id);
 });
-
-// FUNCTIONS
-
-var sc = {
-  dataByUrl: function (ScUrl, callback) {
-    var ScAppUrl = "soundcloud://tracks:";
-    var ApiUrl = 
-      'http://api.soundcloud.com/resolve.json?' + 
-      'url=' + 'http://soundcloud.com' + ScUrl + 
-      '&client_id=' + process.env.SC_client_id;
-  
-    console.log("Requesting " + ApiUrl);
-  
-    request({
-        url: ApiUrl,
-        json: true
-      },
-      function (err, apires, data) {
-      
-        // console.log(apires, data);
-      
-        // check for errors, abort if any
-        if (err || !data ) {
-          return callback("SoundCloud API error :(");
-        }
-        if (data.kind !== "track") {
-          return callback("Input is not a SoundCloud track :(");
-        }
-        if (!data.id) {
-          return callback("Could not find SoundCloud track id :(");
-        }
-        
-        data.appUrl = ScAppUrl + data.id;
-        callback(null, data);
-      
-      }
-    );
-    
-  }
-};
 
 var fail = function (res, str) {
   var msg = '<h1><kbd><b>404:</b> '+str+'</kbd></h1>';
